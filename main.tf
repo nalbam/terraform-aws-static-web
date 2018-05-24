080642a72c26db2656ba17843a3ccda1477a0f72
nalbam <me@nalbam.com>
2018-05-24 13:03:38 +0900

    TLSv1.1_2016

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_cloudfront_distribution" "default" {
 
   viewer_certificate {
     acm_certificate_arn = "${var.certificate_arn}"
-    minimum_protocol_version = "TLSv1"
+    minimum_protocol_version = "TLSv1.1_2016"
     ssl_support_method = "sni-only"
   }
 }
