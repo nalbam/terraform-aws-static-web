3c97c0d3eec99298c8d96806c94108e89ad3edfb
nalbam <me@nalbam.com>
2020-05-21 17:27:29 +0900

    terraform-aws-route53-alias?ref=v0.12.3

--- 53-route53.tf
+++ 53-route53.tf
@@ -1,12 +1,27 @@
 # route53
 
 module "domain" {
-  source = "github.com/nalbam/terraform-aws-route53-alias?ref=v0.12.2"
+  source = "github.com/nalbam/terraform-aws-route53-alias?ref=v0.12.3"
 
   zone_id = var.zone_id
 
-  name = element(var.domain_name, 0)
+  name = var.domain_name
 
   alias_name    = aws_cloudfront_distribution.this.domain_name
   alias_zone_id = aws_cloudfront_distribution.this.hosted_zone_id
 }
+
+# resource "aws_route53_record" "this" {
+#   count = length(var.domain_name)
+
+#   zone_id = var.zone_id
+
+#   name = element(var.domain_name, count.index)
+#   type = "A"
+
+#   alias {
+#     name                   = aws_cloudfront_distribution.this.domain_name
+#     zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+#     evaluate_target_health = "false"
+#   }
+# }
