f64cba9c4de20420f3f02f5adf12ba16f4cd8380
nalbam <me@nalbam.com>
2022-11-09 11:28:52 +0000

    terraform-aws-route53-alias?ref=v1.0.1

--- 53-route53.tf
+++ 53-route53.tf
@@ -1,7 +1,7 @@
 # route53
 
 module "domain" {
-  source = "github.com/nalbam/terraform-aws-route53-alias?ref=v0.13.1"
+  source = "github.com/nalbam/terraform-aws-route53-alias?ref=v1.0.1"
 
   zone_id = var.zone_id
 
@@ -10,18 +10,3 @@ module "domain" {
   alias_name    = aws_cloudfront_distribution.this.domain_name
   alias_zone_id = aws_cloudfront_distribution.this.hosted_zone_id
 }
-
-# resource "aws_route53_record" "this" {
-#   count = length(var.domain_name)
-
-#   zone_id = var.zone_id
-
-#   name = element(var.domain_name, count.index)
-#   type = "A"
-
-#   alias {
-#     name                   = aws_cloudfront_distribution.this.domain_name
-#     zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
-#     evaluate_target_health = "false"
-#   }
-# }
