ef02295b51b09a2c3dc93168907d44054c9146d2
nalbam <me@nalbam.com>
2022-11-09 11:08:37 +0000

    rm website_endpoint

--- 99-output.tf
+++ 99-output.tf
@@ -4,9 +4,9 @@ output "domain" {
   value = element(var.domain_name, 0)
 }
 
-output "s3_website_endpoint" {
-  value = aws_s3_bucket.this.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   value = aws_s3_bucket.this.website_endpoint
+# }
 
 output "cdn_domain" {
   value = aws_cloudfront_distribution.this.domain_name
