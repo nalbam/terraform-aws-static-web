38326e5617080b38b4f8dbf3e06c75290d9a3a5e
nalbam <me@nalbam.com>
2024-08-16 15:55:46 +0900

    chore: Update AWS S3 bucket configuration and ownership controls

--- 32-bucket.tf
+++ 32-bucket.tf
@@ -6,14 +6,8 @@ resource "aws_s3_bucket" "this" {
   force_destroy = var.force_destroy
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.bucket
-
-  acl = var.acl
-}
-
 resource "aws_s3_bucket_website_configuration" "this" {
-  bucket = aws_s3_bucket.this.bucket
+  bucket = aws_s3_bucket.this.id
 
   index_document {
     suffix = var.website_index
@@ -31,4 +25,47 @@ resource "aws_s3_bucket_website_configuration" "this" {
   #     replace_key_prefix_with = "documents/"
   #   }
   # }
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  acl = var.acl
+
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+
+  depends_on = [ aws_s3_bucket_public_access_block.this ]
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
 }
--- 99-output.tf
+++ 99-output.tf
@@ -4,10 +4,6 @@ output "domain" {
   value = element(var.domain_name, 0)
 }
 
-# output "s3_website_endpoint" {
-#   value = aws_s3_bucket.this.website_endpoint
-# }
-
 output "cdn_domain" {
   value = aws_cloudfront_distribution.this.domain_name
 }
@@ -15,3 +11,7 @@ output "cdn_domain" {
 output "cdn_zone_id" {
   value = aws_cloudfront_distribution.this.hosted_zone_id
 }
+
+# output "s3_website_endpoint" {
+#   value = aws_s3_bucket.this.website_endpoint
+# }
