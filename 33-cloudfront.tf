d6bd552191f411312d61a8810c7f055b5cc1f214
nalbam <me@nalbam.com>
2024-08-16 16:34:18 +0900

    chore: Update AWS CloudFront distribution configuration

--- 33-cloudfront.tf
+++ 33-cloudfront.tf
@@ -7,11 +7,25 @@ resource "aws_cloudfront_origin_access_identity" "this" {
 resource "aws_cloudfront_distribution" "this" {
   origin {
     origin_id   = "S3-${local.domain_name}"
-    domain_name = "${local.domain_name}.s3-website.${var.region}.amazonaws.com"
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = [
+        "SSLv3",
+        "TLSv1",
+        "TLSv1.1",
+        "TLSv1.2",
+      ]
     }
+
+    # s3_origin_config {
+    #   origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    # }
   }
 
   aliases = var.domain_name
